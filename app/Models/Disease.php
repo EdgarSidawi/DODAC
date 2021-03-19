e442a98eddc36374a904fbb1cc15176e5478379a
Edgar <romantibuai@gmail.com>
2021-03-19 21:57:41 +0000

    created disease district relationship

--- app/Models/Disease.php
+++ app/Models/Disease.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Disease extends Model
 {
     use HasFactory;
+
+    public function district(){
+        return $this->belongsTo(District::class);
+    }
 }
