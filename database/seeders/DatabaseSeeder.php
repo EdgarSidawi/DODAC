bc5ecda146bfb9940e9cfa2ea67aee29c86ebaa8
Edgar <romantibuai@gmail.com>
2021-03-23 18:22:33 +0000

    created region seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Patient;
+use App\Models\Region;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +20,6 @@ class DatabaseSeeder extends Seeder
 
         User::factory(10)->create();
         Patient::factory(10)->create();
+        Region::factory(10)->create();
     }
 }
