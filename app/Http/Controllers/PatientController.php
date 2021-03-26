577a39a90c67f3aeb53c494b84115a50f598b92d
Edgar <romantibuai@gmail.com>
2021-03-26 16:41:23 +0000

    patient controller@update updated

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -51,14 +51,8 @@ class PatientController extends Controller
      */
     public function update(Request $request, Patient $patient)
     {
-        $patient->update([
-            'firstName' => $request->firstName,
-            'lastName' => $request->lastName,
-            'dateOfBirth' => $request->dateOfBirth,
-            'allergies' => $request->allergies,
-            'last_disease_diagnosed' => $request->last_disease_diagnosed,
-            'last_visited_hospital' => $request->last_visited_hospital,
-        ]);
+        $patient->update($request->all());
+
         return response('Patient updated successfully');
     }
 
