d541c854cf7615c37e145aca7dc1db8968efedf4
Edgar <romantibuai@gmail.com>
2021-03-30 20:04:42 +0100

    Patient feature test user can get a patient

--- tests/Feature/PatientTest.php
+++ tests/Feature/PatientTest.php
@@ -2,21 +2,61 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\PatientController;
+use App\Models\Patient;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PatientTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_can_get_all_patients()
+    {
+        $response = $this->get("/api/patient");
+
+        $response->assertSuccessful()
+            ->assertStatus(200)
+            ->assertJsonCount(1);
+    }
+
+    // /** @test */
+    // public function user_can_create_patient()
+    // {
+    //     $data = [
+    //         'firstName' => 'Howardlyy',
+    //         'lastName' => 'Kemmer',
+    //         'dateOfBirth' => 12 / 06 / 1989,
+    //         'allergies' => 'allergic to bee sting',
+    //         'last_disease_diagnosed' => 'malaria'
+    //     ];
+
+    //     $headers = [
+    //         'Accept' => 'application/json',
+    //         'Content-Type' => 'application/json',
+    //     ];
+
+    //     $response = $this->post(
+    //         "/api/patient",
+    //         $data,
+    //         $headers
+    //     );
+
+    //     $response->assertSuccessful()
+    //         ->assertStatus(200);
+    // }
+
+    /** @test */
+    public function user_can_get_a_patient()
+    {
+        $patient = Patient::factory()->create();
+
+        $response = $this->get("/api/patient/{$patient->id}");
+
+        $response->assertSuccessful()
+            ->assertStatus(200);
     }
 }
