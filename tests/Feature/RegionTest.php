f7e9e0ec6e9df05d47fd6cfd0d53038786f0d39f
Edgar <romantibuai@gmail.com>
2021-03-30 18:08:40 +0100

    Region feature test user can get a region and create region

--- tests/Feature/RegionTest.php
+++ tests/Feature/RegionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Region;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,6 +16,33 @@ class RegionTest extends TestCase
     {
         $response = $this->get('/api/region');
 
-        $response->assertSuccessful()->assertStatus(200)->assertJsonCount(1);
+        $response->assertSuccessful()
+            ->assertStatus(200)
+            ->assertJsonCount(1);
+    }
+
+
+    /** @test */
+    public function user_can_create_region()
+    {
+        $response = $this->post('/api/region', [
+            'name' => 'foo'
+        ]);
+
+        $response->assertSuccessful()
+            ->assertStatus(200);
+    }
+
+
+    /** @test */
+    public function user_can_get_a_region()
+    {
+        $region = Region::factory()->create();
+
+        $response = $this->get("/api/region/{$region->id}");
+
+        $response->assertSuccessful()
+            ->assertStatus(200)
+            ->assertJsonCount(1);
     }
 }
