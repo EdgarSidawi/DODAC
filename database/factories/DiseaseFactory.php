2ada69225f717672a22b91b3b498874bf4278284
Edgar <romantibuai@gmail.com>
2021-03-30 16:56:42 +0100

    DiseaseTest disease has name and threshold attribute

--- database/factories/DiseaseFactory.php
+++ database/factories/DiseaseFactory.php
@@ -24,7 +24,7 @@ class DiseaseFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'district_id'=> District::all()->random()->id,
+            'district_id' => District::all()->random()->id,
             'threshold' => random_int(1, 10),
             'current' => random_int(0, 10)
         ];
--- tests/Unit/DiseaseTest.php
+++ tests/Unit/DiseaseTest.php
@@ -2,17 +2,48 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Disease;
+use App\Models\District;
+use App\Models\Region;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class DiseaseTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function disease_has_name_attribute()
     {
-        $this->assertTrue(true);
+        $region = Region::factory()->create();
+        $district = $region->district()->create([
+            'name' => 'bar'
+        ]);
+        $disease = $district->disease()->create([
+            'name' => 'foo',
+            'threshold' => 10,
+            'current' => 1
+        ]);
+
+        $this->assertEquals($disease->name, $disease->name);
+        $this->assertNotEmpty($disease->name);
+    }
+
+    /** @test */
+    public function disease_has_threshold_attribute()
+    {
+        $region = Region::factory()->create();
+        $district = $region->district()->create([
+            'name' => 'bar'
+        ]);
+        $disease = $district->disease()->create([
+            'name' => 'foo',
+            'threshold' => 10,
+            'current' => 1
+        ]);
+
+        $this->assertEquals(10, $disease->threshold);
+        $this->assertNotEmpty($disease->threshold);
     }
 }
