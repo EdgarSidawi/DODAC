c90b324a41a5e9706c748a1e8c74e3752494b7d0
Edgar <romantibuai@gmail.com>
2021-03-31 21:28:12 +0100

    Disease feature test user can get all diseases of a district

--- tests/Feature/DiseaseTest.php
+++ tests/Feature/DiseaseTest.php
@@ -2,21 +2,26 @@
 
 namespace Tests\Feature;
 
+use App\Models\Disease;
+use App\Models\District;
+use App\Models\Region;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DiseaseTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function user_can_get_all_diseases_of_a_district()
+    {
+        $region = Region::factory()->create();
+        $district = District::factory()->create(['region_id' => $region->id]);
+        $disease = Disease::factory()->count(5)->create(['district_id' => $district->id]);
+
+        $response = $this->get("/api/district/{$district->id}/disease");
+
+        $response->assertSuccessful()->assertJsonCount(1);
     }
 }
