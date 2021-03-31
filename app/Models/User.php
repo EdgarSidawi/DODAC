920897fcd8c2cd547e9a59507042827d7bf96fde
Edgar <romantibuai@gmail.com>
2021-03-31 22:41:24 +0100

    updated user model fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'firstName',
+        'lastName',
+        'username',
+        'role',
         'email',
         'password',
     ];
