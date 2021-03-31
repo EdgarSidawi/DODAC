a06e9f0672f98efd827ad548027d3f5c32714a0a
Edgar <romantibuai@gmail.com>
2021-03-31 18:27:40 +0100

    District feature test user can get all districts of a region

--- tests/Feature/DistrictTest.php
+++ tests/Feature/DistrictTest.php
@@ -2,21 +2,24 @@
 
 namespace Tests\Feature;
 
+use App\Models\District;
+use App\Models\Region;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DistrictTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function user_can_get_all_districts_of_a_region()
+    {
+        $region = Region::factory()->create();
+        $district = District::factory()->count(3)->create(['region_id' => $region->id]);
+
+        $response = $this->get("/api/region/{$region->id}/district");
+
+        $response->assertSuccessful()->assertJsonCount(1);
     }
 }
