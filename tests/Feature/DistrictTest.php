58b2f5d2b9d31f53b72279c70670d5ce36367125
Edgar <romantibuai@gmail.com>
2021-03-31 20:48:09 +0100

    District feature test user can update and delete districts of a region

--- tests/Feature/DistrictTest.php
+++ tests/Feature/DistrictTest.php
@@ -33,4 +33,38 @@ class DistrictTest extends TestCase
 
         $response->assertSuccessful()->assertStatus(200);
     }
+
+    /** @test */
+    public function user_can_get_a_district_of_a_region()
+    {
+        $region = Region::factory()->create();
+        $district = District::factory()->create(['region_id' => $region->id]);
+
+        $response = $this->get("/api/region/{$region->id}/district/{$district->id}");
+
+        $response->assertSuccessful()->assertStatus(200);
+    }
+
+    /** @test */
+    public function user_can_update_a_district_of_a_region()
+    {
+        $region = Region::factory()->create();
+        $district = District::factory()->create(['region_id' => $region->id]);
+        $data = ['name' => 'foo'];
+
+        $response = $this->put("/api/region/{$region->id}/district/{$district->id}", $data);
+
+        $response->assertSuccessful()->assertStatus(200);
+    }
+
+    /** @test */
+    public function user_can_delete_a_district_of_a_region()
+    {
+        $region = Region::factory()->create();
+        $district = District::factory()->create(['region_id' => $region->id]);
+
+        $response = $this->delete("/api/region/{$region->id}/district/{$district->id}");
+
+        $response->assertSuccessful()->assertStatus(200);
+    }
 }
