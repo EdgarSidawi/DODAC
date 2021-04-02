0a09762280983272e3dab851bfaa4b6ec9c1a30b
Edgar <romantibuai@gmail.com>
2021-04-02 14:30:25 +0100

    user unit test user has firstname attribute

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,17 +2,27 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_has_firstName_attribute()
     {
-        $this->assertTrue(true);
+        $user = User::create([
+            'firstName' => 'foo',
+            'lastName' => 'bar',
+            'username' => 'too',
+            'role' => 'nurse',
+            'email' => 'me@example.com',
+            'password' => 'password'
+        ]);
+
+        $this->assertEquals('foo', $user->firstName);
+        $this->assertNotEmpty($user->firstName);
     }
 }
