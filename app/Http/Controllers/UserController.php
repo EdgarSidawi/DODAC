1496ca0128cee8104336843708b3316126f849bb
Edgar <romantibuai@gmail.com>
2021-04-08 20:10:25 +0100

    created UserRequest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function login(Request $request)
+    public function login(UserRequest $request)
     {
-        $data = $request->validate([
-            'username' => 'required',
-            'password' => 'required'
-        ]);
-
         $user = User::where('username', $request->username)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
