5713ac3825d6e48aac475c0765b7ca0a02bd2a28
Edgar <romantibuai@gmail.com>
2021-08-05 15:32:00 +0100

    get all districts route

--- routes/api_guest.php
+++ routes/api_guest.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\DistrictController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/signup', [UserController::class, 'signup']);
+Route::get('/allDistricts', [DistrictController::class, 'allDistricts']);
